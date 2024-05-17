0e804042dff4a298eb151689582cc1d3fa45308e
linuslarrabee <sun_h452@yahoo.com>
2024-05-17 11:14:33 +0800

    feat: flink

--- src/main/java/flink/WordCount.java
+++ src/main/java/flink/WordCount.java
@@ -1,15 +1,14 @@
 package flink;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.util.Collector;
 
 public class WordCount {
 
     public static void main(String[] args) throws Exception {
-
         // 设置执行环境
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
